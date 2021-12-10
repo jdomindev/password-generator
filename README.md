b2d480660384149ad971eaa8aea2fefeef241d7a
Jose Dominguez <jdomindev@gmail.com>
2021-12-09 22:37:25 -0500

    reworked if statements in buildpassword function, added README info

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# password-generator
+# <Password-Generator>
+## Description
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+- Not Applicable
+## Usage
+To use this generator simply press the generate button and then answer the prompts that appear on screen. Your generated password should appear in the middle on the application.
+Deployed link:
+Repo link: 
+Screenshot:
+    ![Screenshot](./assets/images/screenshot.png)
+## Credits
+- Thanks to my sister Maria Dominguez for her insightful comments and on guiding me through my many failed attempts.
+- Also thanks to Adam Khoury for the great video on understanding the functionality behind the Fisher-Yates shuffle. Link to his video here: https://www.youtube.com/watch?v=tLxBwSL3lPQ
+## License
+MIT License
+
+Copyright (c) 2021 Jose
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const lowercaseCharacters = [
+var lowercaseCharacters = [
   "a",
   "b",
   "c",
@@ -29,7 +29,7 @@ const lowercaseCharacters = [
   "y",
   "z",
 ];
-const uppercaseCharacters = [
+var uppercaseCharacters = [
   "A",
   "B",
   "C",
@@ -57,7 +57,7 @@ const uppercaseCharacters = [
   "Y",
   "Z",
 ];
-const specialCharacters = [
+var specialCharacters = [
   "!",
   '"',
   "#",
@@ -91,45 +91,158 @@ const specialCharacters = [
   "}",
   "~",
 ];
-const numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Need to have prompt asking how many characters for password (8-128)
 //    if statement to determine whether answer was <8 or >128
 // Need prompt to ask would you like to include LC,UC,SC,NUM (yes or no)
 //     if statement to determine if < 1 was selected as yes
-// Figure out which types were selected
-//      use those variables to concat arrays into random string
-function buildPassword(isLowercase, isUppercase, isNumber, isSpecial, characterLength) {
-// 0. declare string variable var = password
-// if (isLowercase) meet conditions entered by user
-// const randomLowercaseChar = math.floor(Math.random()) * (lowercaseCharacters.length - 1)
-// password.push(lowercaseCharacters[randomLowercaseChar])
+// write function to make string:
+//
+//      Figure out which types were selected as true from array housing confirm prompt input
+//      Then run loop stating
+//      division count will be number to take from each of the characters arrays
 
-// if (isUppercase) meet conditions entered by user
-// const randomUppercaseChar = math.floor(Math.random()) * (uppercaseCharacters.length - 1)
-// password.push(uppercaseCharacters[randomUppercaseChar])
+// function to handle selecting character types user selects and output string
+function buildPassword(
+  isLowercase,
+  isUppercase,
+  isNumber,
+  isSpecial,
+  characterCount
+) {
+  var combinedArray = [];
+  var conditions = [isLowercase, isUppercase, isNumber, isSpecial];
+  var conditionsCount = 0;
 
-// if (isNumber) meet conditions entered by user
-// const randomNumberChar = math.floor(Math.random()) * (numbersCharacters.length - 1)
-// password.push(numberCharacters[randomNumberChar])
+  for (i = 0; i < conditions.length; i++) {
+    console.log(conditions[i]);
+    if (conditions[i]) {
+      conditionsCount++;
+    }
+  }
+  // divisionCount is number of characters to take from each array of characters
+  var divisionCount = (characterCount) / conditionsCount;
+  var isDecimal = divisionCount % 1 < 0;
+  // var isDecimalGreater = divisionCount % 1 >= .25;
 
-// if (isSpecial) meet conditions entered by user
-// const randomSpecialChar = math.floor(Math.random()) * (specialCharacters.length - 1)
-// password.push(specialCharacters[randomSpecialChar])
+  if (isDecimal) {
+    Math.floor(divisionCount) + 1;
+    console.log(divisionCount);
+  }
 
-// 2. add filler characters based on characterCount and passwordLength
+  // if (isDecimalGreater) {
+  //   Math.floor(divisionCount) - 1;
+  // }
 
-// 3. var arrayIndex = math.floor(Math.random) * lowercaseCharacters.length
-// 4. 
+  // else if (conditionsCount >= 3) {
+  //   Math.floor(divisionCount) - 1;
+  // }
+
+  console.log("conditionsCount", conditionsCount);
+  console.log("characterCount", characterCount);
+  console.log(characterCount / conditionsCount);
+
+  if (isLowercase) {
+    for (i = 0; i < divisionCount; i++) {
+      var randomLowercaseChar = Math.floor(
+        Math.random() * (lowercaseCharacters.length - 1)
+      );
+      combinedArray.push(lowercaseCharacters[randomLowercaseChar]);
+    }
+  }
+  if (isUppercase) {
+    for (i = 0; i < divisionCount; i++) {
+      var randomUppercaseChar = Math.floor(
+        Math.random() * (uppercaseCharacters.length - 1)
+      );
+      combinedArray.push(uppercaseCharacters[randomUppercaseChar]);
+    }
+  }
+  if (isNumber) {
+    for (i = 0; i < divisionCount; i++) {
+      var randomNumberChar = Math.floor(
+        Math.random() * (numberCharacters.length - 1)
+      );
+      combinedArray.push(numberCharacters[randomNumberChar]);
+    }
+  }
+  if (isSpecial) {
+    for (i = 0; i < divisionCount; i++) {
+      var randomSpecialChar = Math.floor(
+        Math.random() * (specialCharacters.length - 1)
+      );
+      combinedArray.push(specialCharacters[randomSpecialChar]);
+    }
+  }
+  // function to shuffle combinedArray (Fisher-Yates Shuffle)
+  function shuffle(combinedArray) {
+  var i = combinedArray.length, j , temp;
+  while(--i > 0) {
+    j = Math.floor(Math.random() * (i+1));
+    temp = combinedArray[j];
+    combinedArray[j] = combinedArray[i];
+    combinedArray[i] = temp;
+  }
+  }
+  shuffle(combinedArray)
+  var passwordFinal = combinedArray.join("");
+  console.log(passwordFinal.length);
+  return passwordFinal;
 }
 
+//   if (isLowercase) {
+//     // meet conditions entered by user
+//     var randomLowercaseChar =
+//       Math.floor(Math.random()) * (lowercaseCharacters.length - 1);
+//     // selects random number in array based on length on variable's array
+//     combinedArray.push(lowercaseCharacters[randomLowercaseChar]);
+//   }
+//   // pushes that random value from array onto combinedArray
 
+//   if (isUppercase) {
+//     var randomUppercaseChar =
+//       Math.floor(Math.random()) * (uppercaseCharacters.length - 1);
+//     combinedArray.push(uppercaseCharacters[randomUppercaseChar]);
+//   }
+
+//   if (isNumber) {
+//     var randomNumberChar =
+//       Math.floor(Math.random()) * (numberCharacters.length - 1);
+//     combinedArray.push(numberCharacters[randomNumberChar]);
+//   }
+
+//   if (isSpecial) {
+//     var randomSpecialChar =
+//       Math.floor(Math.random()) * (specialCharacters.length - 1);
+//     combinedArray.push(specialCharacters[randomSpecialChar]);
+//   }
+
+//   var fillerCharacterCount = characterCount - combinedArray.length;
+//   console.log(fillerCharacterCount);
+//   for (var i = 0; i < fillerCharacterCount; i++) {
+//     console.log(i);
+
+//   }
+
+// // push creates combined array,
+// // array will only have as many characters as types selected
+// //
+// //
+// // combinedArray holds random characters,
+// // 2. add filler characters based on characterCount
+// // loop is based on characterCount aka i
+// //  then turn that array into a string
+// //
 
 function generatePassword() {
+  // Prompt to select how many characters password will be
   var characterCount = window.prompt(
-    "How many characters would you like your password to be? (Select a number between 8 and 128 characters)"
+    "How many characters would you like your password to be? (Must be between 8 and 128 characters)"
   );
+  // if statement makes sure user types in a number between 7 and 129
   if (!(isNaN(characterCount) || characterCount < 8 || characterCount > 128)) {
+    // prompts to select character types
     var isLowercase = window.confirm(
       "Would you like to include lowercase characters?"
     );
@@ -140,20 +253,21 @@ function generatePassword() {
     var isSpecial = window.confirm(
       "Would you like to include special characters?"
     );
-    if (
-      isLowercase ||
-      isUppercase ||
-      isNumber ||
-      isSpecial 
-    ) {
-      return buildPassword(isLowercase, isUppercase, isNumber, isSpecial);
+    // if statement makes sure user selects at least one character type
+    if (isLowercase || isUppercase || isNumber || isSpecial) {
+      //return function to handle building password from character types user selects
+      return buildPassword(
+        isLowercase,
+        isUppercase,
+        isNumber,
+        isSpecial,
+        characterCount
+      );
     } else {
-      console.log("Please select at least one character type.");
-      // change to alert
+      window.alert("Please select at least one character type.");
     }
   } else {
-    console.log("Please enter a number between 8 and 128");
-    // change to alert
+    window.alert("Please enter a number between 8 and 128")
   }
 }
 
